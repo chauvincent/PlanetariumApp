2b9781a23b7b469129f710ef6fd52b83991f26f5
Vincent Chau <vichau@ucdavis.edu>
2015-07-03 23:34:58 -0700

    updated

---
+++ AddObjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddObjectViewController.h
+//  SpaceTableView
+//
+//  Created by Vincent Chau on 6/29/15.
+//  Copyright (c) 2015 Vincent Chau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddObjectViewController : UIViewController
+
+@end
---
+++ AddObjectViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddObjectViewController.m
+//  SpaceTableView
+//
+//  Created by Vincent Chau on 6/29/15.
+//  Copyright (c) 2015 Vincent Chau. All rights reserved.
+//
+
+#import "AddObjectViewController.h"
+
+@interface AddObjectViewController ()
+
+@end
+
+@implementation AddObjectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
